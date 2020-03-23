f013a1679728e4e9dba4dfa4e66b862cda80f821
Tobiasz Jarosiewicz <tjarosiewicz@pc2cis.cis.gov.pl>
2020-03-23 13:14:33 +0100

    Adding -f option.

--- 2_file_output/skrypt.sh
+++ 2_file_output/skrypt.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 if [ "$#" -lt "2" ]
-
 then
   echo "usage aasdasd"
   exit 1
 fi
+
+if [ ! $1 == "-f" ]
+then
+  echo "usage 2"
+  exit 2
+fi
+
 
 echo "Saving output to " $2
 echo "Lorem ipsum" > $2
